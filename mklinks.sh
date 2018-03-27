f725ba64deac9ee0e857b96688fdb3f8a8afe00c
Daniel Popescu <dpopes@yelp.com>
2018-03-27 15:36:33 -0700

    Add more to mklinks to handle the one-off htoprc file

--- mklinks.sh
+++ mklinks.sh
@@ -11,3 +11,16 @@ for f in $(find . -type f -name ".*"); do
     echo "Linking ~/$f to $(pwd)/$f..."
     ln -s $(pwd)/$f ~/$f
 done
+
+# Other one-off files or folders
+htoprc=.config/htop/htoprc
+if [ -a ~/$htoprc ]; then
+    echo "File ~/$htoprc exists... Renaming it!"
+    mv ~/$htoprc ~/$htoprc.bak
+fi
+if [ -h ~/$htoprc ]; then
+    echo "File ~/$htoprc exists and is a symbolic link... Renaming it!"
+    mv ~/$htoprc ~/$htoprc.bak
+fi
+echo "Linking ~/$htoprc to $(pwd)/$htoprc..."
+mkdir ln -s -f -d $(pwd)/.config/htop/htoprc ~/.config/htop/htoprc
