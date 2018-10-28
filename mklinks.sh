d6f882029a36b542a955bbe192ca37f3cd8e6c51
Daniel Popescu <danielpops@gmail.com>
2018-10-28 01:37:07 +0000

    typo derp

--- mklinks.sh
+++ mklinks.sh
@@ -23,5 +23,5 @@ if [ -h ~/$htoprc ]; then
     mv ~/$htoprc ~/$htoprc.bak
 fi
 echo "Linking ~/$htoprc to $(pwd)/$htoprc..."
-mkdir -p ~/.config/htop 2> /dev/nul
+mkdir -p ~/.config/htop 2> /dev/null
 ln -s -f -d $(pwd)/.config/htop/htoprc ~/.config/htop/htoprc
